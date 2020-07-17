839f79bc65b8c60cabd347b6588504b83b732bf2
ZhenyuanLau <zhenyuanlau@icloud.com>
2020-07-17 14:47:57 +0800

    bundle gem

---
+++ lib/growing/ruby/sdk.rb
@@ -0,0 +1,10 @@
+require "growing/ruby/sdk/version"
+
+module Growing
+  module Ruby
+    module Sdk
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/growing/ruby/sdk/version.rb
@@ -0,0 +1,7 @@
+module Growing
+  module Ruby
+    module Sdk
+      VERSION = "0.1.0"
+    end
+  end
+end
